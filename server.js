5a59231629d440379e58d4f7e68412c966796552
Richard Leung <michelleli@MMac.local>
2020-11-18 04:27:17 -0500

    results and detail route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+//var request = require('request');
+const axios = require('axios').default;
+const ejsLayouts = require('express-ejs-layouts');
+const path = require('path'); // for path
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -11,17 +15,44 @@ app.use(express.static('static'));
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Adds some logging to each request
-app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev')); ////???????????????? what is this
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/results', (req, res) => {
+  const search = req.query.search;
+  // Make a request for a user with a given ID
+  axios.get(`http://www.omdbapi.com/?s=${search}&apikey=${process.env.API_KEY}`)
+    .then(function (response) {
+      const results = response.data.Search
+      res.render('results', { results, search })
+    })
+    .catch((error) => {
+      res.send(error);
+    })
+});
+
+app.get('/movies/:imdbID', (req, res) => {
+  const imdbId = req.params.imdbID;
+
+  axios.get(`http://www.omdbapi.com/?i=${imdbId}&apikey=${process.env.API_KEY}`)
+    .then(function (response) {
+      const details = response.data;
+      res.render('detail', { details })
+    })
+    .catch((error) => {
+      res.send(error);
+    })
 });
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
