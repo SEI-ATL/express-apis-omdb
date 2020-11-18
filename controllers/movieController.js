3ebad1607154e4d24bb83e0b65f22bec050dc1b7
Your Name <you@example.com>
2020-11-17 20:31:09 -0500

    stuck

---
+++ controllers/movieController.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const fetch = require("node-fetch");
+const { default:axios } = require('axios')
+// const search = 'godfather';
+
+const movieRouter = require('express').Router()
+
+movieRouter.get('/', function(req, res){
+    res.render('index')
+
+})
+movieRouter.get('/:titleSearch', function(req, res){
+    res.render('index')
+    const search = req.params.titleSearch
+    console.log(search)
+    axios.get(`http://www.omdbapi.com/?apikey=${api_key}&s=${search}`)
+    // .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response.data)))
+    // .then(res.render('/results'))
+    
+    
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = movieRouter
--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const layouts = require('express-ejs-layouts');
 const fetch = require("node-fetch");
 const dotenv = require('dotenv').config();
 const search = 'godfather';
+
+const movieRouter = require('./controllers/movieController')
 api_key = process.env.api_key
 
-console.log(process.env); 
+// console.log(process.env); 
 
 fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${search}`)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => console.log(response.title))
+
+app.set('view engine', 'ejs')
+app.use(layouts)
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/', function(req, res){
+    res.send('hello I am alive!')
+})
+
+app.use('/movies', movieRouter)
+
+app.listen(8000, () => {
+    console.log('server started!')
+})
