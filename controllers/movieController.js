d654b272f980c558f5892e6df70d6a06dba57447
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-18 05:32:31 -0500

    [installed, set up axios as well as api key]

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,9 +1,15 @@
+const { default: Axios } = require('axios')
 const fs = require('fs')
 
 const movieRouter = require('express').Router()
 
 movieRouter.get('/', (req, res)=>{
     res.render('index')
+})
+
+movieRouter.get('/:title', (req, res)=>{
+    const mySearch = req.body.title;
+    Axios.get( `http://www.omdbapi.com/?i=${mySeatch}&apikey=${process.env.API_KEY}`
 })
 
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
