399762f68d8de4ac80a1cd318e581cea7cdae6f4
Jenny <finej01@gmail.com>
2020-11-18 00:00:15 -0500

    updated details + results pages

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-var request = require('request');
+//var request = require('request');
 const axios = require('axios').default;
 const ejsLayouts = require('express-ejs-layouts');
+const path = require('path'); // for path
+
 const app = express();
 
 // Sets EJS as the view engine
@@ -13,6 +15,7 @@ app.use(express.static('static'));
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 // Adds some logging to each request
 app.use(require('morgan')('dev')); ////???????????????? what is this
@@ -30,8 +33,9 @@ app.get('/results', (req, res) => {
     .then(function (response) {
       // handle success
       //res.send(response.data.Search)
+      const title = `${response.data.Search.length} Matches Found for '${query}'`;
       res.render('results', {
-        title: 'Results',
+        title,
         results: response.data.Search,
       });
     })
@@ -48,15 +52,17 @@ app.get('/movies/:movie_id', (req, res) => {
     .get(`https://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${movieid}`)
     .then(function (response) {
       const title = `Movie Details: ${movieid}`;
-      res.render('detail', { title, details: response.data});
-      console.log(response.data)
+      //use a reduce to reduce the obj
+      const star = "⭐";
+      const rating = star.repeat(Math.floor(response.data.imdbRating));
+      res.render('detail', { title, details: response.data, rating});
+      console.log(response.data);
     })
     .catch((error) => {
       res.send('Bad Link');
       console.log(error);
     });
 });
-
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
