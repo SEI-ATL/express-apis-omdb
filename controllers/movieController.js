10dcfefafe5f703a1c07508ddab56bf09245726d
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-18 08:53:15 -0500

    data accessed from api

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,5 +1,6 @@
 const movieRouter = require('express').Router();
-
+const axios = require('axios');
+const API_KEY = '29fb6c49';
 
 //routes
 movieRouter.get('/', (req, res) => {
@@ -7,8 +8,14 @@ movieRouter.get('/', (req, res) => {
 });
 
 movieRouter.get('/results', (req, res) => {
-    console.log(req.query);
-    res.render('results', { title: req.query.title });
+    let searchQuery = req.query.title;
+    axios.get(`http://www.omdbapi.com/?t=${searchQuery}&apikey=${API_KEY}`, (res) => {
+        let movieData = res.json.data;
+    }).then((movieData) => {
+        console.log(movieData.data.Title);
+        res.render('results', {data: movieData.data})
+    });
+
 });
 
 
