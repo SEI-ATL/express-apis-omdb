d59990fccef30226b1f9c623146ba8402f69c843
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-11-18 00:07:26 -0500

    Deliverable basically complete, just need some clean up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios').default;
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,11 +19,45 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
+  console.log('Hello');
 });
 
+
+
+
+
+app.get('/results', (req, res) => {
+  console.log(req.query);
+
+  // for (const key in req.query) {
+  //   console.log(key, req.query[key])
+  // }
+  
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.api}&s=${req.query.q}`)
+  .then(function (response) {
+    const movies = response.data.Search
+    console.log(movies);
+    
+    res.render('results', { movies })
+  })
+})
+
+app.get('/movies/:movie_id', (req,res) => {
+  const movie_id = req.params.movie_id
+  console.log(movie_id)
+
+  axios.get(`http://www.omdbapi.com/?apikey=${process.env.api}&i=${movie_id}`)
+  .then(function (response) {
+    console.log(response.data)
+    const movie = response.data
+
+    res.render('detail', { movie })
+  })
+})
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
 
 // We can export this server to other servers like this
 module.exports = server;
+
