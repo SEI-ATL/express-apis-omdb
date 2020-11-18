47e219494fd2033bae73da2a9dcc4817cb26b33b
Edree Huss <edreeshuss@gmail.com>
2020-11-18 13:07:17 -0500

    [Completed] Clean up code, add styling sheet. Fix ratings.

--- server.js
+++ server.js
@@ -1,37 +1,64 @@
 require('dotenv').config();
 const express = require('express');
+const axios = require('axios').default;
 const ejsLayouts = require('express-ejs-layouts');
+const path = require('path'); 
+
 const app = express();
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 
 app.use(express.static('static'));
-// Sets up body-parser for parsing form data
+
 app.use(express.urlencoded({ extended: false }));
 // Enables EJS Layouts middleware
 app.use(ejsLayouts);
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-// Adds some logging to each request
-app.use(require('morgan')('dev'));
 
+app.use(require('morgan')('dev')); 
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
+app.get('/results', (req, res) => {
+  const query = req.query.q;
+  // Make a request for a user with a given ID
+  axios
+    .get(`https://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=${query}`)
+    .then(function (response) {
+ 
+      const title = `${response.data.Search.length} Matches Found for '${query}'`;
+      res.render('results', {
+        title,
+        results: response.data.Search,
+      });
+    })
+    .catch((error) => {
+      res.send('Bad Search');
+      console.log(error);
+    });
+});
+app.get('/movies/:movie_id', (req, res) => {
+  const movieid = req.params.movie_id;
+  axios
+    .get(`https://www.omdbapi.com/?apikey=${process.env.API_KEY}&i=${movieid}`)
+    .then(function (response) {
+      const title = `Movie Details: ${movieid}`;
 
-app.listen(8000, () => {
-    console.log('server started')
-}) 
+      const star = "*";
+      const rating = star.repeat(Math.floor(response.data.imdbRating));
+      res.render('detail', { title, details: response.data, rating});
+      console.log(response.data);
+    })
+    .catch((error) => {
+      res.send('Error');
+      console.log(error);
+    });
+});
+
+
+// The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
-
-
-module.exports = server;
-
-
-
-
-
-
-
 
