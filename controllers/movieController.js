b3797265d1bd665de408442354933172bf91a029
Your Name <you@example.com>
2020-11-18 18:37:04 -0500

    added working details page

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -14,17 +14,27 @@ movieRouter.get('/results', function(req, res){
     console.log(title)
     axios.get(`http://www.omdbapi.com/?apikey=${api_key}&s=${title}`)
     .then((response) => {console.log((response.data.Search))
+        const iD = response.data.Search.imbID
         const results = response.data.Search
     res.render('results', { results })
+    console.log("results", results)
     })
     // .then(res.render('/results'))
     
-    
-    
-    
 })
 
+movieRouter.get('/details/:movieId', function(req, res){
+    // const title = req.query.movieDetail
+    const movieId = req.params.movieId;
 
+    axios.get(`http://www.omdbapi.com/?apikey=${api_key}&i=${movieId}`)
+    .then((response) => {console.log("response", response.data)
+    const movieDetails = response.data
+    console.log("movieDetails", movieDetails)
+    res.render('detail', {movieDetails})
+    console.log(movieId)
+})
+})
 
 
 
