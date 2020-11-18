ee2d94f8043546c26b94bb5cc4863d57ed16da00
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2020-11-18 05:54:18 -0500

    [search engine]

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,13 +4,20 @@ const fs = require('fs')
 const movieRouter = require('express').Router()
 
 movieRouter.get('/', (req, res)=>{
-    res.render('index')
+    res.render('movies/index')
 })
 
 movieRouter.get('/:title', (req, res)=>{
     const mySearch = req.body.title;
-    Axios.get( `http://www.omdbapi.com/?i=${mySeatch}&apikey=${process.env.API_KEY}`
-})
+    Axios.get( `http://www.omdbapi.com/?i=${mySeatch}&apikey=${process.env.API_KEY}`)
+    .then ((response) => {
+        res.render('movies/results'), 
+        {movies: response.data.Search}
+    })
+    .catch((error)=> {
+        console.log(error);
+    })
+});
 
 
 module.export = movieRouter
