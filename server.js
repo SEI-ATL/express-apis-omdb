47cee9d9fdf2ee35989efa27bdd7a12add53c98f
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-18 12:03:33 -0700

    working queries

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
-const axios = require('axios')
 const app = express();
+const axios = require('axios')
+const apiKey = process.env.API_KEY
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -21,13 +22,34 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/results', function(req, res) {
-  res.render('results')
-})
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
-app.get('/movies/:movie_id', function(req, res) {
-  res.render('details')
-})
+app.get('/results', (req, res) => {
+  const query = req.query.q;
+  axios
+    .get(`https://www.omdbapi.com/?apikey=64e0f9b4&s=${query}`)
+    .then(function (response) {
+      const title = `${response.data.Search.length} Matches Found for '${query}'`;
+      res.render('results', {
+        title,
+        results: response.data.Search,
+      });
+    })
+});
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movieid = req.params.movie_id;
+
+  axios
+    .get(`https://www.omdbapi.com/?apikey=64e0f9b4&i=${movieid}`)
+    .then(function (response) {
+      const title = `Movie Details: ${movieid}`;
+      res.render('detail', { title, details: response.data });
+      console.log(response.data);
+    })
+});
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
