8e5bfe8203f6a5b75f5c0a9d02e7cf4dfe184f33
sschneeberg <simone.m.schneeberg@gmail.com>
2020-11-18 00:32:48 -0500

    Add routes to app

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
+const axios = require('axios');
+const apiKey = process.env.API_KEY;
+
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -17,11 +20,29 @@ app.use(require('morgan')('dev'));
 
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+    res.render('index')
 });
 
+app.get('/results', (req, res) => {
+    let title = req.query.q;
+    axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
+        .then((data) => {
+            let results = data.data.Search;
+            res.render('results', { results, title });
+        })
+})
+
+
+app.get('/movies/:movie_id', (req, res) => {
+    axios.get(`http://www.omdbapi.com/?i=${req.params.movie_id}&apikey=${apiKey}`)
+        .then((data) => {
+            let movie = data.data;
+            res.render('detail', { movie });
+        })
+})
+
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, () => console.log('start'));
 
 // We can export this server to other servers like this
 module.exports = server;
