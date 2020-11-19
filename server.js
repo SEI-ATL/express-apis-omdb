d5fb7092de9589330304f8874166271771700084
Nitish Dayal <contact@nitishdayal.me>
2020-11-19 07:02:25 -0800

    feat: Part 1 complete

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const axios = require('axios')
+
+
 const app = express();
+const { API_KEY: apikey } = process.env
+
+if (!process.env.API_KEY) {
+  console.warn('WARNING: APP WILL FAIL WITHOUT OMDB API KEY!')
+  console.log('Create .env file and add api key using format "API_KEY=<your key>"')
+}
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,12 +25,40 @@ app.use(ejsLayouts);
 app.use(require('morgan')('dev'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello, backend!');
-});
+app.get('/', function (_, res) {
+  res.render('index');
+})
+
+app.get('/results', (req, res) => {
+  const s = req.query.q
+  const qs = { params: { s, apikey } }
+
+  axios.get(`http://www.omdbapi.com/`, qs)
+    .then(({ data: { Search } }) => {
+      const movies = Search
+        .filter(query => query.Type === 'movie')
+        .sort((a, b) => parseInt(a.Year) - parseInt(b.Year))
+
+      res.render('results', { movies })
+    }).catch(e => { console.error(e) })
+})
+
+app.get('/movies/:movieId', (req, res) => {
+  const { movieId: i } = req.params
+  const qs = { params: { i, apikey } }
+
+  axios.get('http://www.omdbapi.com', qs)
+    .then(({ data }) => {
+      const details = data
+
+      res.render('detail', { details })
+    })
+})
 
 // The app.listen function returns a server handle
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT || 3000}`)
+});
 
 // We can export this server to other servers like this
 module.exports = server;
