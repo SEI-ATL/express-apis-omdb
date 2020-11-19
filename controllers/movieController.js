04a842c0e35e7262d54cc60e1f6fccd5e038be23
Thomas Gilbert <tcgilbert94@gmail.com>
2020-11-18 21:03:58 -0500

    listing faves on faves route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,6 +1,6 @@
+const db = require('../models');
 const movieRouter = require('express').Router();
 const axios = require('axios');
-
 
 //routes
 movieRouter.get('/', (req, res) => {
@@ -9,8 +9,8 @@ movieRouter.get('/', (req, res) => {
 
 movieRouter.get('/results', (req, res) => {
     let searchQuery = req.query.title;
-    axios.get(`http://www.omdbapi.com/?s=${searchQuery}&apikey=${process.env.API}`, (res) => {
-        let movieData = res.json.data;
+    axios.get(`http://www.omdbapi.com/?s=${searchQuery}&apikey=${process.env.API}`, (omdbRes) => {
+        let movieData = omdbRes.json.data;
     }).then((movieData) => {
         res.render('results', {data: movieData.data.Search})
     });
@@ -18,13 +18,29 @@ movieRouter.get('/results', (req, res) => {
 
 movieRouter.get('/movies/:movie_id', (req, res) => {
     let searchId = req.params.movie_id;
-    axios.get(`http://www.omdbapi.com/?i=${searchId}&apikey=${process.env.API}`, (res) => {
-        let movieData = res.json;
+    axios.get(`http://www.omdbapi.com/?i=${searchId}&apikey=${process.env.API}`, (omdbRes) => {
+        let movieData = omdbRes.json;
     }).then((movieData) => {
+        console.log(movieData.data);
         res.render('detail', { data: movieData.data });
     })
 });
 
+movieRouter.get('/faves', (req, res) => {
+    db.faves.findAll().then(dbRes => {
+        res.render('faves', { favesArray: dbRes });
+    })
+})
 
+movieRouter.post('/faves', (req, res) => {
+    db.faves.create({
+        title: req.body.title,
+        imdbid: req.body.imdbid
+    }).then(dbRes => {
+        console.log(dbRes.get());
+    }).then(() => {
+        res.redirect('/faves');
+    });
+})
 
 module.exports = movieRouter;
