e9ee9159b38fdba5bf7d1fb5cd72972e7a0e4b03
Will Locklier <will.locklier@gmail.com>
2020-11-19 10:35:36 -0500

    [server.js] Add completed pat 2 files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
-const movieRouter = require('./controller/movieController');
 const app = express();
 const axios = require('axios')
+const apiKey = '9db3ace8'
+const db = require('./models')
 
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
@@ -16,11 +17,43 @@ app.use(ejsLayouts);
 
 // Adds some logging to each request
 app.use(require('morgan')('dev'));
-app.use('/', movieRouter)
+
 // Routes
 app.get('/', function(req, res) {
-  res.send('Hello, backend!');
+  res.render('index');
 });
+app.get('/results', (req, res) => {
+  let search = req.query.q;
+  axios.get(`http://www.omdbapi.com/?s=${search}&apikey=${apiKey}`)
+      .then((searchResults) => {
+          let results = searchResults.data.Search;
+          res.render('results', { results, search });
+      })
+})
+
+
+app.get('/movies/:imdbid', (req, res) => {
+  axios.get(`http://www.omdbapi.com/?i=${req.params.imdbid}&apikey=${apiKey}`)
+      .then((data) => {
+          let movie = data.data;
+          res.render('detail', { movie });
+      })
+})
+app.post('/faves', (req, res) => {
+  db.fave.create(req.body).then((createdFave) => {
+    res.redirect('/faves')
+  })
+})
+app.get('/faves', (req, res) => {
+  db.fave.findAll().then((foundFaves) => {
+    res.render('faves', { faves: foundFaves })
+  })
+
+})
+
+
+
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
