c120ccb11dc912b7e4cb16fa0fbac0966a16d8e2
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-19 08:33:06 -0700

    adding faves

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const axios = require('axios')
 const apiKey = process.env.API_KEY
 
+const models = require('./models')
+
 // Sets EJS as the view engine
 app.set('view engine', 'ejs');
 // Specifies the location of the static assets folder
@@ -22,9 +24,6 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
 
 app.get('/results', (req, res) => {
   const query = req.query.q;
@@ -41,15 +40,27 @@ app.get('/results', (req, res) => {
 
 app.get('/movies/:movie_id', (req, res) => {
   const movieid = req.params.movie_id;
-
   axios
-    .get(`https://www.omdbapi.com/?apikey=64e0f9b4&i=${movieid}`)
+    .get(`https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${movieid}`)
     .then(function (response) {
       const title = `Movie Details: ${movieid}`;
       res.render('detail', { title, details: response.data });
       console.log(response.data);
     })
 });
+
+app.post('/faves/', (req, res) => {
+  models.fave.create(req.body).then((createdFave => {
+    res.redirect('/faves')
+  })
+})
+
+app.get('/faves', (req, res) => {
+  models.fave.findAll().then((foundFaves) => {
+    res.render('faves', { faves: foundFaves} )
+  })
+})
+
 
 // The app.listen function returns a server handle
 var server = app.listen(process.env.PORT || 3000);
